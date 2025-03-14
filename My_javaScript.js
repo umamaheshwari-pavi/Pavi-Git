10598df0167dc2231f40cc4e922dade92a7a1878
umamaheshwari-pavi <umapavi200012@gmail.com>
2025-03-14 19:29:55 +0530

    inserted my javascript file in pavi-git

---
+++ My_javaScript.js
@@ -0,0 +1,10 @@
+// My_javaScript.js
+
+// Function to greet the user
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to your JavaScript file.`;
+}
+
+// Example usage
+const userName = 'Pavi';
+console.log(greetUser(userName));
